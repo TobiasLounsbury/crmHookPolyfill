f853e20a28969805f0e5dc1a58f5147367931db6
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2017-01-12 15:29:36 -0800

    Added promise versioin of run, as well as handler weighting mechanism

--- ang/crmHook.js
+++ ang/crmHook.js
@@ -4,21 +4,62 @@
       'crmUtil'
     ]);
   angular.module('crmUtil').factory('crmHook', [
-    function() {
+    function($q) {
       var listeners = {};
       return {
-        runHook: function(hookName, args) {
+        runHook: function(hookName, args, type) {
           if (listeners.hasOwnProperty(hookName)) {
-            listeners[hookName].forEach(function (cb) {
-              cb(args);
+            var value;
+            listeners[hookName].forEach(function (listener) {
+              switch (type) {
+                case "first":
+                  return listener.callback(args);
+                case "last":
+                  value = listener.callback(args);
+                  break;
+                case "all":
+                  value.push(listener.callback(args));
+                  break;
+                default:
+                  listener.callback(args);
+              }
             });
+            return value;
           }
+          return false;
         },
-        register: function(hookName, callback) {
+        //type: void, first, last, all
+        promiseHook: function(hookName, args, type, failOnNoHooks) {
+          var deferred = $q.defer();
+          var that = this;
+          setTimeout(function() {
+          if (listeners.hasOwnProperty(hookName)) {
+            deferred.resolve(that.runHook(hookName, args, type));
+          } else {
+            if(failOnNoHooks) {
+              deferred.reject("No Hooks Registered");
+            } else {
+              deferred.resolve();
+            }
+          }
+          }, 25);
+
+          return deferred.promise;
+        },
+        register: function(hookName, callback, weight) {
           if (!listeners.hasOwnProperty(hookName)) {
             listeners[hookName] = [];
           }
-          listeners[hookName].push(callback);
+          weight = parseInt(weight) || 100 + listeners[hookName].length;
+
+          listeners[hookName].push({"callback": callback, "weight": weight});
+          if(listeners[hookName].length > 1) {
+            //Sort listeners by weight.
+            listeners[hookName].sort(function(a, b) {
+              return a.weight - b.weight;
+            });
+
+          }
         }
       };
     }
